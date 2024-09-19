ad94eafdc0fe3b6594d825765b57cc05c9a6b146
alexanderdombroski <dom21013@byui.edu>
2024-09-19 17:42:20 -0600

    added js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 import { getParkData } from "./parkService.mjs";
 
+// Get Park Data
 const parkData = getParkData();
+
+(function insertParkData() {
+    // Update Disclaimer
+    const realSiteLink = document.querySelector(".disclaimer > a");
+    realSiteLink.href = parkData.url;
+    realSiteLink.innerText = parkData.fullName;
+
+    // Update Hero
+    const hero = document.getElementById("hero-banner");
+    const heroImage = hero.querySelector("img");
+    heroImage.src = parkData.images[0].url;
+    heroImage.alt = parkData.images[0].altText;
+    const heroText = hero.querySelector(".hero-banner-text");
+    heroText.innerHTML = heroTextTemplate(parkData);
+})();
+
+
+function heroTextTemplate(parkInfo) {
+    return (
+    `<a href="#">${parkInfo.name}</a>
+    <p>
+        <span>${parkInfo.designation}</span>
+        <span>${parkInfo.states}</span>
+    </p>`
+    );
+}
+
