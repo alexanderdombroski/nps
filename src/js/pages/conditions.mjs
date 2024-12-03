a6b16441f7d1b7173add93bf2abaa4289d3280fe
alexanderdombroski <dom21013@byui.edu>
2024-12-03 10:17:11 -0700

    moved main js scripts for pages to pages folder

---
+++ src/js/pages/conditions.mjs
@@ -0,0 +1,31 @@
+import { fetchParkData, fetchAlertData, fetchVisitorCenterData } from "../data/fetchData.mjs";
+import { insertAlerts, insertFooter, insertParkHeader, insertActivites, insertVisitorCenters } from "../render.mjs";
+import { globalMenuListenerSetup, globalSubMenuListenerSetup } from "../listeners.mjs";
+
+(async function loadPage() {
+    const parkData = await fetchParkData();
+    const alertData = await fetchAlertData();
+    const visitorCenterData = await fetchVisitorCenterData();
+
+    insertParkHeader(parkData);
+    insertAlerts(alertData);
+    insertFooter(parkData);
+    insertVisitorCenters(visitorCenterData);
+    insertActivites(parkData.activities);
+})();
+
+(function initListeners() {
+    globalMenuListenerSetup();
+    globalSubMenuListenerSetup();
+})();
+
+// autoCloseToggles
+document.getElementById("main").querySelectorAll("details").forEach(dropdown => {
+    dropdown.addEventListener('toggle', function (event) {
+        if (event.target.open === false) return; 
+        document.getElementById("main").querySelectorAll("details").forEach(other => other.open = other === dropdown);
+        dropdown.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+});
+
+
---
+++ src/js/pages/main.mjs
@@ -0,0 +1,22 @@
+import { getParkInfoLinks, getParkData } from "../data/parkService.mjs";
+import { insertParkHeader, insertParkInfo, insertParkIntro, insertFooter } from "../render.mjs";
+import { fetchParkData } from "../data/fetchData.mjs";
+import { globalMenuListenerSetup, globalSubMenuListenerSetup } from "../listeners.mjs";
+
+
+(async function loadInfo() {
+    // Get Data
+    const parkData = await fetchParkData();
+    const parkInfoLinks = getParkInfoLinks(parkData);
+
+    // Manipulate HMTL
+    insertParkHeader(parkData);
+    insertParkIntro(parkData);
+    insertParkInfo(parkInfoLinks);
+    insertFooter(parkData);
+})();
+
+(function initListeners() {
+    globalMenuListenerSetup();
+    globalSubMenuListenerSetup();
+})();
